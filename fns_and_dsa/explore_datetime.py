e21a22f5f0c28c877413bd4855858b8a95662859
Jackline <jacqyridants@gmail.com>
2025-06-08 21:55:54 +0300

    Fix input prompt to match required format

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -9,7 +9,7 @@ def display_current_datetime():
 
 def calculate_future_date():
     try:
-        days = int(input("Enter the number of days to add: "))
+    days = int(input("Enter the number of days to add to the current date:S"))
         current_date = datetime.now()
         future_date = current_date + timedelta(days=days)
         print("Future Date:", future_date.strftime("%Y-%m-%d"))
